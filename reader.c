b950025990a898e35e58f0f5b5a09aad976e1137
Prajin19 <yelloveforever197@gmail.com>
2024-10-08 09:48:06 +0530

    Success

--- reader.c
+++ reader.c
@@ -1,4 +1,3 @@
-// C Program for Message Queue (Reader Process)
 #include <stdio.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
@@ -14,13 +13,13 @@ int main()
 	int msgid;
 // ftok to generate unique key
 	key = ftok("progfile", 65);
-	// msgget creates a message queue
+// msgget creates a message queue
 	// and returns identifier
-	msgid = 
+	msgid = msgget(key, 0666 | IPC_CREAT);
 	// msgrcv to receive message
-	
+	msgrcv(msgid, &message, sizeof(message), 1, 0);
 	// display the message
-	printf("Data Received is : %s \n",
+printf("Data Received is : %s \n",
 			message.mesg_text);
 
 	// to destroy the message queue
